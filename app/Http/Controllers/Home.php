16e057692b7f2fdc83686f6db793088f022de74a
AlfonsoLoopez <loopezalfonso@gmail.com>
2023-05-14 15:57:13 -0600

    Creando metodos para rutas de vistas

--- app/Http/Controllers/Home.php
+++ app/Http/Controllers/Home.php
@@ -6,5 +6,47 @@ use Illuminate\Http\Request;
 
 class Home extends Controller
 {
-    //
+
+    public function index() {
+        $news = News::all();
+        return view('index', compact('news'));
+    }
+
+    public function certificaciones() {
+        $news = News::all();
+        return view('certificaciones', compact('news'));
+    }
+
+    public function productos() {
+        $productos = Product::all();
+        $news = News::all();
+        return view('navbar.productos', compact('productos', 'news'));
+    }
+
+    public function contactanos() {
+        $news = News::all();
+        return view('navbar.contactanos', compact('news'));
+    }
+
+    public function servicios() {
+        $fields = Field::all();
+        $news = News::all();
+        return view('navbar.servicios', compact('fields', 'news'));
+    }
+
+    public function noticias() {
+        $news = News::all();
+        return view('noticias', compact('news'));
+    }
+
+    public function clientes() {
+        $news = News::all();
+        return view('clientes', compact('news'));
+    }
+
+    public function quienes_somos() {
+        $news = News::all();
+        return view('quienes_somos', compact('news'));
+    }
+
 }
