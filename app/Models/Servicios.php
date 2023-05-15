33712930e7baeecbd55a3aba3f5f4532bb744233
AlfonsoLoopez <loopezalfonso@gmail.com>
2023-05-14 21:47:53 -0600

    modelo de servicios

--- app/Models/Servicios.php
+++ app/Models/Servicios.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Servicios extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function field() {
+        return $this->belongsTo(Field::class)->withTrashed();
+    }
 }
